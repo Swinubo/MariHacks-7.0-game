fd0f8ea37867aa2216ce797acb781fabb91bd4e1
Swinubo <gasparmendes66@gmail.com>
2024-05-26 17:46:28 -0400

    npc optimization

--- Scripts/NPC.cs
+++ Scripts/NPC.cs
@@ -6,7 +6,7 @@ using System.Threading;
 
 public class NPC : MonoBehaviour
 {
-    private string gartner = "Gartner: I like apples, do you?";
+    /*private string gartner = "Gartner: I like apples, do you?";
     private string fox = "Fox: Aaaaaaargh! Woops, I'm just scared of my own name.";
     private string piper = "Piper: For the last time, I am NOT a Brawl Stars character!";
     private string anderson = "Annderson: Uhhhh, what I do... I play with CR7!";
@@ -16,9 +16,10 @@ public class NPC : MonoBehaviour
     private string mbappee = "Mbappee: Don't walk on grass! Catalans will attack you!";
     private string renchesse = "Renchesse: I'm a woman???";
     private string logan = "Logan: Did you know you snore at night?";
-    private string mom = "Mom: Hey son! Glad to see you back! Go explore the world of Monsters of Dawn!";
+    private string mom = "Mom: Hey son! Glad to see you back! Go explore the world of Monsters of Dawn!";*/
+    [SerializeField] private string npc_text;
 
-    [SerializeField] private Text text_displ;
+    private Text text_displ;
     [SerializeField] private bool mumInScene;
     private Rigidbody2D rb;
     private Rigidbody2D rbMum;
@@ -27,6 +28,8 @@ public class NPC : MonoBehaviour
 
     private void Start()
     {
+        text_displ = GameObject.Find("text").GetComponent<Text>(); 
+
         rb = GameObject.Find("Player").GetComponent<Rigidbody2D>();
         if (mumInScene)
         {
@@ -43,58 +46,17 @@ public class NPC : MonoBehaviour
             GameObject.Find("General_text").GetComponent<Image>().enabled =true; 
             GameObject.Find("General_text").GetComponent<Button>().enabled =true; 
 
-            if (gameObject.name == "NPC_Gartner")
-            {
-                text_displ.text = gartner;
-            }
-            else if (gameObject.name == "NPC_Fox")
-            {
-                text_displ.text = fox;
-            }
-            else if (gameObject.name == "NPC_Piper")
-            {
-                text_displ.text = piper;
-            }
-            else if (gameObject.name == "NPC_Anderson")
-            {
-                text_displ.text = anderson;
-            }
-            else if (gameObject.name == "NPC_Baifield")
-            {
-                text_displ.text = baifield;
-            }
-            else if (gameObject.name == "NPC_Joseph")
-            {
-                text_displ.text = joseph;
-            }
-            else if (gameObject.name == "NPC_Alexander")
-            {
-                text_displ.text = alexander;
-            }
-            else if (gameObject.name == "NPC_Mbappee")
-            {
-                text_displ.text = mbappee;
-            }
-            else if (gameObject.name == "NPC_Renchesse")
-            {
-                text_displ.text = renchesse;
-            }
-            else if (gameObject.name == "NPC_Logan")
-            {
-                text_displ.text = logan;
-            }
-            else if (gameObject.name == "NPC_Mom")
+            text_displ.text = npc_text;
+            if (gameObject.name == "NPC_Mom")
             {
                 StartCoroutine(DisplayMomText());         
-            }
-            
+            }      
         }
     }
 
     //mom
     IEnumerator DisplayMomText()
     {
-        text_displ.text = mom;
         rb.bodyType = RigidbodyType2D.Static;
         yield return new WaitForSeconds(3);
         anim.SetBool("leave", true);
