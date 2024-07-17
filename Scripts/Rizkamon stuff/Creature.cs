78a32266a49c126c93298a14a342f190aad7c606
Swinubo <gasparmendes66@gmail.com>
2024-07-17 17:09:36 -0400

    idk what i did (again)

--- Scripts/Rizkamon stuff/Creature.cs
+++ Scripts/Rizkamon stuff/Creature.cs
@@ -1,12 +1,12 @@
 public class Creature
 {
     //Properties of creature
-    public string Name { get; private set; }
-    public int Health { get; private set; }
-    public float Attack { get; private set; }
-    public float Defence { get; private set; }
-    public Move Move1 { get; private set; }
-    public Move Move2 { get; private set; }
+    public string Name { get; set; }
+    public int Health { get; set; }
+    public float Attack { get; set; }
+    public float Defence { get; set; }
+    public Move Move1 { get; set; }
+    public Move Move2 { get; set; }
 
     //Creatures
     public static Creature Bip;
@@ -41,8 +41,8 @@ public class Creature
         Move2 = move2;
     }
 
-    public override string ToString()
+    /*public override string ToString()
     {
         return $"Creature: {Name}, Health: {Health}, Attack: {Attack}, Defence: {Defence}, Move1: {Move1.Name}, Move2: {Move2.Name}";
-    }
+    }*/
 }
